abae449e2d4e2ecaecb6f40027f6db19214e24ba
Karanjot Singh <99573351+Karanjot786@users.noreply.github.com>
2023-01-28 18:58:17 +0530

    add

--- news/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-admin.site.site_header = "Balanced News Admin"
-admin.site.site_title = "Balanced News Admin Area"
-admin.site.index_title = "Welcome to the Balanced News Admin Area"
